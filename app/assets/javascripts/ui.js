59a2912e69cf57f7c8beea0248f86a4f5d023785
Connor Kojimoto <cokojimoto@gmail.com>
2015-01-06 11:49:16 -0800

    added check answer/get request and created edit page

--- app/assets/javascripts/ui.js
+++ app/assets/javascripts/ui.js
@@ -15,20 +15,39 @@ $(function() {
        $('.quizzes-display').append($el);
     });
   });
-});
 
-$(function() {
+
   $('.quest-form').on('click','.quest-submit',function(e) {
     var $quiz = $('.quest-value').val();
     $.post("/quizzes",{"quiz[title]": $quest },function(status) {
       // console.log(status);
     });
   })
-})
 
-$(function() {
+
   $('.quizzes-display').on('click','.quiz-inspect',function(e) {
     e.preventDefault();
+    $('.quest-form').html("");
+    var $id = $(this).data("id");
+    var $title = $(this).html();
+    console.log($id + " " + $title);
+    $('.quizzes-display').html("");
+    var template = $('.quiz-edit-template').html();
+    var uncompiledTemplate = _.template(template);
+    var compiledTemplate = uncompiledTemplate({
+        content: {
+          title: $title,
+          id:$id
+        }
+    });
+    var $el = $(compiledTemplate);
+    $('.quizzes-display').append($el);
+
+
+
+    $('.quizzes-display').on('click','.quiz-inspect',function(e) {
+    e.preventDefault();
+
     var $quest = $(this).data('id');
     $('.quest-form').html("");
     $('.quizzes-display').html("");
@@ -59,84 +78,38 @@ $(function() {
        dispQuest(cnt);
        $('.question-display').on('click','.quest-submit',function(e) {
           e.preventDefault();
-            
+          
             var ans = $('.quest-option').val();
+            $.get('/quizzes/' + $quest + '/questions/' + data[cnt].id + "/check?answer=" + ans,function(res) {
+              console.log(res.correct);
+              if (res.correct) {
+                score++;
+                $('.question-display').append("<p>Correct!</p>");
+              } else {
+                $('.question-display').append("<p>Incorrect.</p>");
+              }
 
-            if (ans === data[cnt].answer) {
-              score++;
-              $('.question-display').append("<p>Correct!</p>");
-            } else {
-              $('.question-display').append("<p>Incorrect.</p>");
-            }
+              if (cnt+1 == data.length) {
+                var dispRes = function() {
+                  $('.question-display').html("");
+                  $('.question-display').append('<h3>You Finished with a score of ' + score + '</h3');
+                };
+                setTimeout(dispRes,1000);  
+              } else {
+                cnt++;  
+                setTimeout(function(){ dispQuest(cnt) }, 1000);
+              }  
+            }) 
+          })
+      });          
+    }); 
+  });  
+    
 
-            if (cnt+1 == data.length) {
-              var dispRes = function() {
-                $('.question-display').html("");
-                $('.question-display').append('<h3>You Finished with a score of ' + score + '</h3');
-              };
-              setTimeout(dispRes,1000);  
-            } else {
-              cnt++;  
-              setTimeout(function(){ dispQuest(cnt) }, 1000);
-            }  
-            
-        })
-    });          
- });
   
 });       
 
-        // data.forEach(function(v) {
-        //   var choice = v.choices.split(";");
-        //   console.log(choice);
-        //   var template = $('.quiz-questions-template').html();
-        //   var uncompiledTemplate = _.template(template);
 
-        //   var compiledTemplate = uncompiledTemplate({
-        //     content: {
-        //       question: v.question,
-        //       id: v.id,
-        //       answer: v.answer,
-        //       choices: choice
-        //     }
-        //   });
-
-        //   var $el = $(compiledTemplate);
-        //   console.log($el);
-
-        //   $('.question-display').append($el);
-        // });          
-       
-
-      //  var cnt = 0;
-      //  while (cnt < data.length) {   
-      //     var choice = data[cnt].choices.split(";");
-      //     console.log(choice);
-      //     var template = $('.quiz-questions-template').html();
-      //     var uncompiledTemplate = _.template(template);
-
-      //     var compiledTemplate = uncompiledTemplate({
-      //       content: {
-      //         question: data[cnt].question,
-      //         id: data[cnt].id,
-      //         answer: data[cnt].answer,
-      //         choices: choice
-      //       }
-      //     });
-
-      //     var $el = $(compiledTemplate);
-      //     console.log($el);
-      //     $('.question-display').append($el);
-      //   $('.quizzes-display').on('click','.quest-submit',function(e) {
-      //     e.preventDefault();
-      //     console.log(cnt)
-      //     var $option = $('option').val();
-      //     console.log($option);
-          
-      //     cnt++;
-      //   })
-          
-      // }  
  
   
 
